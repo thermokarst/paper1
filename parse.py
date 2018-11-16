a78e118be1baef8ad8ff7697de8b94e4923b6b94
Matthew Dillon <matthewrdillon@gmail.com>
2018-11-16 10:57:04 -0700

    SQUASH: WIP

--- parse.py
+++ parse.py
@@ -1,22 +1,20 @@
 import yaml
 import glob
 from collections import OrderedDict
+import pprint
 
 import qiime2.plugins
 import importlib
 from qiime2.sdk import Result
 
-
-final_artifact = Result.load('diff.qzv')
-results = {}
-for result in glob.glob('*.qz*'):
-    results[Result.peek(result).uuid] = result
-
+pp = pprint.PrettyPrinter(indent=2)
+pprint = pp.pprint
 
 yaml.add_constructor('!ref', lambda x, y: y)
 yaml.add_constructor('!cite', lambda x, y: y)
+yaml.add_constructor('!metadata', lambda x, y: y)
 
-def get_import(action, prov_dir, results, uuid):
+def get_import(action, prov_dir, uuid):
     cmd = ['qiime', 'tools', 'import', '--input-path']
     assert len(action['action']['manifest']) == 1
     cmd.append(action['action']['manifest'][0]['name'])
@@ -27,25 +25,31 @@ def get_import(action, prov_dir, results, uuid):
         metadata = yaml.load(fh)
     cmd.append(metadata['type'])
     cmd.append('--output-path')
-    cmd.append(results[uuid])
+    cmd.append(str(uuid))
     return ' '.join(cmd)
 
-def get_command(action, results, prov_dir, uuid):
+def get_command(action, prov_dir, uuid):
     if action['action']['type'] == 'import':
-        return get_import(action, prov_dir, results, uuid), []
+        return get_import(action, prov_dir, uuid), []
     cmd = ['qiime']
     cmd.append(action['action']['plugin'].value.split(':')[-1])
     cmd.append(action['action']['action'].replace('_', '-'))
     for param_dict in action['action']['parameters']:
         (param, value), = param_dict.items()
         mod = importlib.import_module(
-            'qiime2.plugins.' + action['action']['plugin'].value.split(':')[-1].replace('-', '_'),
+            'qiime2.plugins.' +
+            action['action']['plugin'].value.split(':')[-1].replace('-', '_'),
         )
+        print('action!')
+        pprint(action['action'])
         parameters = getattr(mod.actions, action['action']['action']).signature.parameters
+        print('params')
+        pprint(parameters)
         if value != parameters[param].default:
             param_sig = parameters[param]
             if 'Metadata' in param_sig.qiime_type.name:
-                assert False
+                cmd.append('--m-' + param.replace('_', '-'))
+                cmd.append(str(value))
             elif param_sig.qiime_type.name == 'Bool':
                 cmd.append('--p-' + ('' if value else 'no-') + param.replace('_', '-'))
             else:
@@ -53,28 +57,35 @@ def get_command(action, results, prov_dir, uuid):
                 cmd.append(str(value))
     required_artifacts = []
     cmd.append('--o-' + action['action']['output-name'].replace('_', '-'))
-    cmd.append(results[uuid])
+    cmd.append(str(uuid))
     for imput in action['action']['inputs']:
-        (imput, uuid), = imput.items()
-        if uuid is None:
-            continue
-        cmd.append('--i-' + imput.replace('_', '-'))
-        cmd.append(results[uuid])
-        required_artifacts.append(uuid)
+        (imput, uuids), = imput.items()
+        uuids = uuids if type(uuids) == list else [uuids]
+        for uuid in uuids:
+            if uuid is None:
+                continue
+            cmd.append('--i-' + imput.replace('_', '-'))
+            cmd.append(str(uuid))
+            print('whoa', uuid)
+            required_artifacts.append(uuid)
     return ' '.join(cmd), required_artifacts
 
-def get_commands(action, results, prov_dir, uuid=None):
-    cmd, dependencies = get_command(action, results, prov_dir, uuid)
+def get_commands(action, prov_dir, uuid=None):
+    cmd, dependencies = get_command(action, prov_dir, uuid)
     commands = [cmd]
+    print('deps', dependencies)
     for uuid in dependencies:
+        print(prov_dir, uuid)
         with (prov_dir / 'artifacts' / uuid / 'action' / 'action.yaml').open() as fh:
             action = yaml.load(fh)
-        commands.extend(get_commands(action, results, prov_dir, uuid))
+        commands.extend(get_commands(action, prov_dir, uuid))
     return commands
 
+
+final_artifact = Result.load('/Users/matthew/src/qiime2/paper1/figure1/a-pcoa.qzv')
 with (final_artifact._archiver.provenance_dir / 'action' / 'action.yaml').open() as fh:
     action = yaml.load(fh)
 
-commands = reversed(get_commands(action, results, final_artifact._archiver.provenance_dir, str(final_artifact.uuid)))
+commands = reversed(get_commands(action, final_artifact._archiver.provenance_dir, str(final_artifact.uuid)))
 for cmd in OrderedDict([(c, None) for c in commands]):
     print(cmd)
